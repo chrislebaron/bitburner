e25fabd6ee741711ffca0de5232c62f46cc54474
Chris LeBaron <chrislebaron@gmail.com>
2022-02-15 15:23:14 -0700

    Update to ns.stock.* functions

--- src/stockMarketer4S.js
+++ src/stockMarketer4S.js
@@ -17,7 +17,7 @@ function getMoney(ns) {
 
 function sellShorts(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const shortSellValue = ns.sellShort(stockSymbol, stockInfo.sharesShort)
+  const shortSellValue = ns.stock.sellShort(stockSymbol, stockInfo.sharesShort)
 
   if (shortSellValue) {
     corpus += stockInfo.sharesShort * (stockInfo.avgPriceShort - shortSellValue) - 2 * commission
@@ -32,7 +32,7 @@ function sellShorts(ns, stockSymbol) {
 
 function sellLongs(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const longSellValue = ns.sellStock(stockSymbol, stockInfo.sharesLong)
+  const longSellValue = ns.stock.sell(stockSymbol, stockInfo.sharesLong)
 
   if (longSellValue) {
     corpus += stockInfo.sharesLong * (longSellValue - stockInfo.avgPriceLong) - 2 * commission
@@ -86,16 +86,17 @@ function buyNewShares(ns, stockSymbol) {
       maxSharesToBuy = stockInfo.maxShares - stockInfo.sharesLong
       sharesToBuy = Math.max(0, Math.min(maxSharesToBuy, Math.floor(getMoney(ns) / stockInfo.stockAskPrice)))
       if (sharesToBuy) {
-        buyValue = ns.buyStock(stockSymbol, sharesToBuy)
+        buyValue = ns.stock.buy(stockSymbol, sharesToBuy)
       }
       shareType = 'longs'
     } else {
-      maxSharesToBuy = stockInfo.maxShares - stockInfo.sharesShort
-      sharesToBuy = Math.max(0, Math.min(maxSharesToBuy, Math.floor(getMoney(ns) / stockInfo.stockBidPrice)))
-      if (sharesToBuy) {
-        buyValue = ns.shortStock(stockSymbol, sharesToBuy)
-      }
-      shareType = 'shorts'
+      /***** UNCOMMENT IF YOU HAVE SOURCE FILE 8 ***********/
+      // maxSharesToBuy = stockInfo.maxShares - stockInfo.sharesShort
+      // sharesToBuy = Math.max(0, Math.min(maxSharesToBuy, Math.floor(getMoney(ns) / stockInfo.stockBidPrice)))
+      // if (sharesToBuy) {
+      //   buyValue = ns.stock.short(stockSymbol, sharesToBuy)
+      // }
+      // shareType = 'shorts'
     }
 
     if (sharesToBuy) {
@@ -105,18 +106,19 @@ function buyNewShares(ns, stockSymbol) {
   }
 }
 
+/** @param {NS} ns **/
 function getStockInfo(ns, stockSymbol) {
-  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.getStockPosition(stockSymbol)
-  const volatility = ns.getStockVolatility(stockSymbol)
-  const probability = ns.getStockForecast(stockSymbol) - 0.5
+  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.stock.getPosition(stockSymbol)
+  const volatility = ns.stock.getVolatility(stockSymbol)
+  const probability = ns.stock.getForecast(stockSymbol) - 0.5
   const expectedReturn = Math.abs(volatility * probability)
-  const maxShares = ns.getStockMaxShares(stockSymbol)
+  const maxShares = ns.stock.getMaxShares(stockSymbol)
 
   const haveAnyShares = sharesLong + sharesShort > 0
   const haveMaxShares = sharesLong + sharesShort === maxShares
 
-  const stockAskPrice = ns.getStockAskPrice(stockSymbol)
-  const stockBidPrice = ns.getStockBidPrice(stockSymbol)
+  const stockAskPrice = ns.stock.getAskPrice(stockSymbol)
+  const stockBidPrice = ns.stock.getBidPrice(stockSymbol)
 
   const position = probability >= 0 ? 'Long' : 'Short'
 
@@ -138,19 +140,21 @@ function getStockInfo(ns, stockSymbol) {
   }
 }
 
+/** @param {NS} ns **/
 export async function main(ns) {
   ns.disableLog('ALL')
   let tickCounter = 1
 
-  stockSymbols = ns.getStockSymbols()
+  stockSymbols = ns.stock.getSymbols()
 
-  corpus = ns.getServerMoneyAvailable('home') - 1000000
+  
+  let portfolioValue = 0
   stockSymbols.forEach((stockSymbol) => {
     const stockInfo = getStockInfo(ns, stockSymbol)
 
-    corpus += stockInfo.sharesLong * stockInfo.avgPriceLong + stockInfo.sharesShort * stockInfo.avgPriceShort
+    portfolioValue += stockInfo.sharesLong * stockInfo.avgPriceLong + stockInfo.sharesShort * stockInfo.avgPriceShort
   })
-  const startingCorpus = corpus
+  const startingCorpus = portfolioValue
 
   while (true) {
     ns.clearLog()
@@ -180,7 +184,9 @@ export async function main(ns) {
     stockSymbols.forEach((stockSymbol) => buyNewShares(ns, stockSymbol))
     await ns.sleep(5)
 
-    ns.print(`[${localeHHMMSS()}] After transactions: corpus: ${ns.nFormat(corpus, '$0.000a')}`)
+    const cashValue = ns.getPlayer().money;
+    const totalValue = portfolioValue + cashValue;
+    ns.print(`Net worth: ${ns.nFormat(totalValue, "$0.000a")} = ${ns.nFormat(portfolioValue, "$0.000a")} stocks + ${ns.nFormat(cashValue, "$0.000a")} cash`);
     await ns.sleep(3500)
     tickCounter++
   }
